5a121f239f94a23f8dccb7cc8c34a3d530026d42
Junaadh <junaadh.02@gmail.com>
2024-06-01 04:18:19 +0800

    more derives

--- core/src/memory/pager/paging.rs
+++ core/src/memory/pager/paging.rs
@@ -1,16 +1,106 @@
 use crate::memory::traits::Unit;
-use r2proc::Unit;
+use r2proc::{Range, Unit};
+use x86_64::{PhysAddr, VirtAddr};
 
 use super::index::PageIndex;
 
 #[derive(Clone, Copy, Unit, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[unit_type(PhysAddr)]
 #[repr(transparent)]
 pub struct Frame {
     index: PageIndex,
 }
 
 #[derive(Clone, Copy, Unit, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[unit_type(VirtAddr)]
 #[repr(transparent)]
 pub struct Page {
     index: PageIndex,
+}
+
+pub struct FrameIterator {
+    current: Frame,
+    limit: Frame,
+}
+
+impl FrameIterator {
+    pub fn new(init: &FrameRange) -> Self {
+        Self {
+            current: init.start,
+            limit: init.end,
+        }
+    }
+}
+
+impl Iterator for FrameIterator {
+    type Item = Frame;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.current > self.limit {
+            true => None,
+            false => {
+                let cur = self.current;
+                self.current += 1;
+                Some(cur)
+            }
+        }
+    }
+}
+
+pub struct PageIterator {
+    current: Page,
+    limit: Page,
+}
+
+impl PageIterator {
+    pub fn new(init: &PageRange) -> Self {
+        Self {
+            current: init.start,
+            limit: init.end,
+        }
+    }
+}
+
+impl Iterator for PageIterator {
+    type Item = Page;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.current > self.limit {
+            true => None,
+            false => {
+                let cur = self.current;
+                self.current += 1;
+                Some(cur)
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy, Range)]
+#[range_types(Frame, FrameIterator)]
+pub struct FrameRange {
+    start: Frame,
+    end: Frame,
+}
+
+#[derive(Clone, Copy, Range)]
+#[range_types(Page, PageIterator)]
+pub struct PageRange {
+    start: Page,
+    end: Page,
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::format;
+
+    use crate::memory::{pager::index::PageIndex, traits::Unit};
+
+    use super::Frame;
+
+    #[test]
+    fn debug() {
+        let frame = Frame::at(PageIndex::new(0));
+        assert_eq!(format!("{:?}", frame), "Frame<0>");
+    }
 }
--- r2proc/src/lib.rs
+++ r2proc/src/lib.rs
@@ -1,16 +1,50 @@
+extern crate proc_macro;
+
+use core::panic;
+
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Token};
 
-#[proc_macro_derive(Unit)]
-pub fn derive_pager(item: TokenStream) -> TokenStream {
+struct OneType(syn::Ident);
+impl syn::parse::Parse for OneType {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let type1 = input.parse()?;
+        Ok(OneType(type1))
+    }
+}
+
+struct TwoTypes(syn::Ident, syn::Ident);
+impl syn::parse::Parse for TwoTypes {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let type1 = input.parse()?;
+        input.parse::<Token![,]>()?;
+        let type2 = input.parse()?;
+        Ok(TwoTypes(type1, type2))
+    }
+}
+
+#[proc_macro_derive(Unit, attributes(unit_type))]
+pub fn derive_unit(item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
 
     let name = &input.ident;
+    let attr = match input
+        .attrs
+        .iter()
+        .find(|attr| attr.path().is_ident("unit_type"))
+        .map(|attr| {
+            // syn::parse2::<OneType>(attr.clone().into_token_stream())
+            attr.parse_args::<OneType>()
+                .expect("Failed to parse attribute")
+        }) {
+        Some(s) => s.0,
+        None => panic!("Expected #[framer_type(/*type*/)]"),
+    };
 
     quote! {
         impl crate::memory::traits::Unit for #name {
-            type Addr = x86_64::VirtAddr;
+            type Addr = #attr;
 
             #[inline]
             fn new(addr: Self::Addr) -> Self {
@@ -23,7 +57,7 @@ pub fn derive_pager(item: TokenStream) -> TokenStream {
             }
 
             #[inline]
-            fn addr(self) -> x86_64::VirtAddr {
+            fn addr(self) -> Self::Addr {
                 Self::Addr::new((self.index.0 * crate::memory::PAGE_SIZE) as u64)
             }
 
@@ -66,8 +100,131 @@ pub fn derive_pager(item: TokenStream) -> TokenStream {
 
         impl core::fmt::Debug for #name {
             fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-                write!(f, "Page<{:x}>", self.addr().as_u64())
+                write!(f, "{}<{:x}>", stringify!(#name), self.addr().as_u64())
             }
+        }
+    }
+    .into()
+}
+
+#[proc_macro_derive(Range, attributes(range_types))]
+pub fn derive_range(item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as DeriveInput);
+    let name = &input.ident;
+
+    let (basic, iter) = match input
+        .attrs
+        .iter()
+        .find(|a| a.path().is_ident("range_types"))
+        .map(|a| {
+            a.parse_args::<TwoTypes>()
+                .expect("Failed to parse arguments")
+        }) {
+        Some(a) => (a.0, a.1),
+        None => panic!("Expected #[range_types(/*type1: Unit*//*type2: Iterator*/)]"),
+    };
+
+    quote! {
+        impl crate::memory::traits::Range for #name {
+            type Basic = #basic;
+            type Iter = #iter;
+
+            #[inline]
+            fn new(start: Self::Basic, end: Self::Basic) -> Self {
+                Self {start, end}
+            }
+
+            #[inline]
+            fn empty() -> Self {
+                Self {
+                    start: Self::Basic::at(PageIndex(1)),
+                    end: Self::Basic::at(PageIndex(0))
+                }
+            }
+
+            #[inline]
+            fn start(self) -> Self::Basic {
+                self.start
+            }
+
+            #[inline]
+            fn end(self) -> Self::Basic {
+                self.end
+            }
+
+            #[inline]
+            fn as_page_size(self) -> usize {
+                (self.end.index.0 - self.start.index.0) + 1
+            }
+
+            #[inline]
+            fn as_bytes(self) -> usize {
+                self.as_page_size() * crate::memory::PAGE_SIZE
+            }
+
+            #[inline]
+            fn is_empty(self) -> bool {
+                self.start > self.end
+            }
+
+            #[inline]
+            fn start_addr(self) -> <Self::Basic as Unit>::Addr {
+                self.start.addr()
+            }
+
+            #[inline]
+            fn inclusive_addr(self) -> <Self::Basic as Unit>::Addr {
+                self.end.inclusive_addr()
+            }
+
+            #[inline]
+            fn merge(&mut self, other: Self) -> Result<(), alloc::boxed::Box<dyn core::error::Error>> {
+                if other.is_empty() {
+                    return Ok(());
+                }
+                if other.start != self.end + 1 && other.end + 1 != self.start {
+                    return Err("Error merging pages".into());
+                }
+                if other.start < self.start {
+                    self.start = other.start;
+                }
+                if other.end > self.end {
+                    self.end = other.end;
+                }
+
+                Ok(())
+            }
+
+            #[inline]
+            fn overlaps(self, other: Self) -> bool {
+                if self.is_empty() ||other.is_empty() {
+                    return false;
+                }
+
+                self.start <= other.end && other.start <= self.end
+            }
+
+            #[inline]
+            fn consumes(self, other: Self) -> bool {
+                if self.is_empty() || other.is_empty() {
+                    return false;
+                }
+                self.start <= other.start && self.end >= other.end
+            }
+
+            #[inline]
+            fn contains(self, unit: Self::Basic) -> bool {
+                if self.is_empty() {
+                    return false;
+                }
+                self.start <= unit && self.end >= unit
+            }
+
+            #[inline]
+            fn iter(&self) -> Self::Iter {
+                Self::Iter::new(self)
+            }
+
         }
     }
     .into()
